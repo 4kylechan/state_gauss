4b6c41b416978fd1351fba5cbb2a1c9e15d119ef
Akif <akifkaplay9@gmail.com>
2026-04-02 23:14:39 +0300

    feat 2D region + event system calisiyor

--- state.py
+++ state.py
@@ -5,6 +5,7 @@ model = YOLO("yolov8n.pt")
 cap = cv2.VideoCapture(0)
 
 prev_state = "no_phone"
+prev_region = "NONE"
 phone_frames = 0
 
 while True:
@@ -18,6 +19,11 @@ while True:
 
     phone_detected_this_frame = False
     region = "NONE"
+    event_code = "none"
+    event_text = ""
+
+    cv2.line(annotated_frame, (w // 2, 0), (w // 2, h), (255, 255, 255), 2)
+    cv2.line(annotated_frame, (0, h // 2), (w, h // 2), (255, 255, 255), 2)
 
     for box in results[0].boxes:
         cls_id = int(box.cls[0])
@@ -57,6 +63,7 @@ while True:
         phone_frames += 1
     else:
         phone_frames = 0
+        region = "NONE"
 
     if phone_frames > 5:
         state = "phone_detected"
@@ -66,39 +73,32 @@ while True:
     if state != prev_state:
         print("STATE CHANGED:", prev_state, "→", state)
 
+    if region != prev_region and region != "NONE" and prev_region != "NONE":
+        if prev_region == "TOP_RIGHT" and region == "TOP_LEFT":
+            event_text = "telefon sag ustten sol uste gecti"
+        elif prev_region == "TOP_LEFT" and region == "TOP_RIGHT":
+            event_text = "telefon sol ustten sag uste gecti"
+        elif prev_region == "BOTTOM_RIGHT" and region == "BOTTOM_LEFT":
+            event_text = "telefon sag alttan sol alta gecti"
+        elif prev_region == "BOTTOM_LEFT" and region == "BOTTOM_RIGHT":
+            event_text = "telefon sol alttan sag alta gecti"
+
+        if event_text != "":
+            print("EVENT:", event_text)
+
     prev_state = state
+    prev_region = region
 
-    cv2.putText(
-        annotated_frame,
-        f"STATE: {state}",
-        (20, 40),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        1,
-        (0, 0, 255),
-        2
-    )
+    cv2.putText(annotated_frame, f"STATE: {state}", (20, 40),
+                cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
 
-    cv2.putText(
-        annotated_frame,
-        f"PHONE FRAMES: {phone_frames}",
-        (20, 80),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        0.8,
-        (255, 0, 0),
-        2
-    )
+    cv2.putText(annotated_frame, f"REGION: {region}", (20, 80),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 0), 2)
 
-    cv2.putText(
-        annotated_frame,
-        f"REGION: {region}",
-        (20, 120),
-        cv2.FONT_HERSHEY_SIMPLEX,
-        0.8,
-        (255, 255, 0),
-        2
-    )
+    cv2.putText(annotated_frame, f"EVENT: {event_text}", (20, 120),
+                cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 255, 255), 2)
 
-    cv2.imshow("YOLO Cell Phone Filter", annotated_frame)
+    cv2.imshow("YOLO", annotated_frame)
 
     if cv2.waitKey(1) & 0xFF == ord("q"):
         break

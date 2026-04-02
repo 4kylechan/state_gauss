3a06b56a81c90228636efc2578bafa5ee1889e11
Akif <akifkaplay9@gmail.com>
2026-04-02 22:25:22 +0300

    feat: basic state logic eklendi

--- state.py
+++ state.py
@@ -2,7 +2,6 @@ from ultralytics import YOLO
 import cv2
 
 model = YOLO("yolov8n.pt")
-
 cap = cv2.VideoCapture(0)
 
 while True:
@@ -11,14 +10,18 @@ while True:
         break
 
     results = model(frame, conf=0.25, verbose=False)
-
     annotated_frame = frame.copy()
+
+    # her frame başında state reset eder babuş
+    state = "no_phone"
 
     for box in results[0].boxes:
         cls_id = int(box.cls[0])
         class_name = model.names[cls_id]
 
         if class_name == "cell phone":
+            state = "phone_detected"
+
             x1, y1, x2, y2 = map(int, box.xyxy[0])
 
             cv2.rectangle(annotated_frame, (x1, y1), (x2, y2), (0, 255, 0), 2)
@@ -31,6 +34,17 @@ while True:
                 (0, 255, 0),
                 2
             )
+
+    # for dışında yazılır asuman 
+    cv2.putText(
+        annotated_frame,
+        f"STATE: {state}",
+        (20, 40),
+        cv2.FONT_HERSHEY_SIMPLEX,
+        1,
+        (0, 0, 255),
+        2
+    )
 
     cv2.imshow("YOLO Cell Phone Filter", annotated_frame)
 
